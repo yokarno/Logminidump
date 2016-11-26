8e9627915cd753f4df05d84adf555b57246da8db
yokarno <spraydew@sina.com>
2016-11-26 09:16:26 +0800

    create code

---
+++ Code/Logminidump/Logminidump.cpp
@@ -0,0 +1,36 @@
+// Logminidump.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <string>
+#include <windows.h>
+
+#include <Iphlpapi.h>
+#include <WinDef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#pragma comment(lib,"Iphlpapi.lib")
+using namespace std;
+
+#include "Logminidump.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    getchar();
+    for (int i = 0;i<100;i ++)
+    {
+        char *szBuffer = new char[100];
+
+        delete szBuffer;
+        szBuffer = NULL;
+
+        int n = strlen(szBuffer);
+
+        char *szBuffer1 = new char[n+100];
+        delete szBuffer1;
+    }
+    getchar();
+	return 0;
+}
+
